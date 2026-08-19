270bd14a73c12dc9e543d5d6328ec91d1f1ac53c
Zhu Xinye <zhuxinye754@gmail.com>
2026-08-19 18:06:50 +0800

    Updated the code to echo commands entered by the user, and exit when the user types the command bye

--- src/main/java/Clover.java
+++ src/main/java/Clover.java
@@ -1,6 +1,9 @@
 /**
  * Starts the Clover chatbot application.
  */
+
+import java.util.Scanner;
+
 public class Clover {
     public static void main(String[] args) {
         String banner = "  _____    _         ____    __      __   ______    _____\n"
@@ -9,13 +12,24 @@ public class Clover {
                 + "| |       | |      | |  | |    \\ \\/ /    |  __|    |  _  /\n"
                 + "| |____   | |____  | |  | |     \\  /     | |____   | | \\ \\\n"
                 + " \\_____|  |______|  \\____/       \\/      |______|  |_|  \\_\\\n";
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(banner);
 
         System.out.println("Hello! I'm Clover.");
         System.out.println("What can I do for you?\n");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
     }
 }
