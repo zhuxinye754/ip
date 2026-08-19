df81b02921ed7bef350066bedbae6220533c39b2
Zhu Xinye <zhuxinye754@gmail.com>
2026-08-19 17:15:34 +0800

    Gave the chatbot a new name – Clover

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Clover User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Clover.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Clover chatbot application.
+ */
+public class Clover {
+    public static void main(String[] args) {
+        String banner = "  _____    _         ____    __      __   ______    _____\n"
+                + " / ____|  | |       / __ \\   \\ \\    / /  |  ____|  |  __ \\\n"
+                + "| |       | |      | |  | |   \\ \\  / /   | |__     | |__) |\n"
+                + "| |       | |      | |  | |    \\ \\/ /    |  __|    |  _  /\n"
+                + "| |____   | |____  | |  | |     \\  /     | |____   | | \\ \\\n"
+                + " \\_____|  |______|  \\____/       \\/      |______|  |_|  \\_\\\n";
+        System.out.println(banner);
+    }
+}
